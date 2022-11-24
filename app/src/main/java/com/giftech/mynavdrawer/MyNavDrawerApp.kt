98dbe0cc8f10fc79edfc92ae00083cb69bfe1232
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-24 19:55:59 +0700

    Initial commit

---
+++ app/src/main/java/com/giftech/mynavdrawer/MyNavDrawerApp.kt
@@ -0,0 +1,17 @@
+package com.giftech.mynavdrawer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.giftech.mynavdrawer.ui.theme.MyNavDrawerTheme
+
+@Composable
+fun MyNavDrawerApp() {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyNavDrawerTheme {
+        MyNavDrawerApp()
+    }
+}
