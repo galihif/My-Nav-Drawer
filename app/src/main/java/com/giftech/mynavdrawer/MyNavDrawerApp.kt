6b1f0500ea5215f9af48a5351f66e90236600bbd
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-24 20:26:27 +0700

    add onclick and back press handler

--- app/src/main/java/com/giftech/mynavdrawer/MyNavDrawerApp.kt
+++ app/src/main/java/com/giftech/mynavdrawer/MyNavDrawerApp.kt
@@ -1,5 +1,8 @@
 package com.giftech.mynavdrawer
 
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,13 +12,13 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,9 +43,29 @@ fun MyNavDrawerApp() {
         },
         drawerContent = {
             MyDrawerContent(
-                onItemSelected = {
-
-                }
+                onItemSelected = { title ->
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                        val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                            message = context.resources.getString(R.string.coming_soon, title),
+                            actionLabel = context.resources.getString(R.string.subscribe_question)
+                        )
+                        if (snackbarResult == SnackbarResult.ActionPerformed) {
+                            Toast.makeText(
+                                context,
+                                context.resources.getString(R.string.subscribed_info),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                },
+                onBackPress = {
+                    if (scaffoldState.drawerState.isOpen) {
+                        scope.launch {
+                            scaffoldState.drawerState.close()
+                        }
+                    }
+                },
             )
         },
         drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
@@ -82,6 +105,7 @@ data class MenuItem(val title: String, val icon: ImageVector)
 fun MyDrawerContent(
     modifier: Modifier = Modifier,
     onItemSelected: (title: String) -> Unit,
+    onBackPress: () -> Unit,
 ) {
     val items = listOf(
         MenuItem(
@@ -122,6 +146,35 @@ fun MyDrawerContent(
             }
         }
         Divider()
+    }
+    BackPressHandler {
+        onBackPress()
+    }
+}
+
+
+@Composable
+fun BackPressHandler(enabled: Boolean = true, onBackPressed: () -> Unit) {
+    val currentOnBackPressed by rememberUpdatedState(onBackPressed)
+    val backCallback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+    SideEffect {
+        backCallback.isEnabled = enabled
+    }
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
+        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
+    }.onBackPressedDispatcher
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, backDispatcher) {
+        backDispatcher.addCallback(lifecycleOwner, backCallback)
+        onDispose {
+            backCallback.remove()
+        }
     }
 }
 
